a4d84c21e3457cab4b61accb703b0c4b537aaac9
harshithaark <amharshithaar@gmail.com>
2024-07-13 13:25:50 +0530

    Create and run server

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/sign-in" element={<Signin/>}/>
