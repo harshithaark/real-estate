e49d7f2a72f4bb37057ef960b542dfd743379361
harshithaark <amharshithaar@gmail.com>
2024-07-13 17:52:00 +0530

    Completed UI for Sign Up

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <h1 className = 'text-3xl text-center font-semibold my-7'>Signup</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder="username" className='border p-3 rounded-lg' id='username'></input>
+        <input type="email" placeholder="email" className='border p-3 rounded-lg' id='email'></input>
+        <input type="password" placeholder="password" className='border p-3 rounded-lg' id='password'></input>
+      </form>
+    </div>
   )
 }
