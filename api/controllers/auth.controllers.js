06d8a7eee8487b7bbb0b5cf020efa0da3e709ac7
harshithaark <amharshithaar@gmail.com>
2024-07-13 17:35:32 +0530

    Xreate a middleware and a function to handle possible errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from '../Models/user.model.js';
 import bcryptjs from 'bcryptjs';
-export const signup = async(req, res) => {
+import { errorHandler } from '../utils/error.js';
+export const signup = async(req, res, next) => {
 
    const { username, email, password} = req.body;
    const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -9,7 +10,7 @@ export const signup = async(req, res) => {
       await newUser.save();
       res.status(201).json("User created sucessfully");
    }catch(error){
-    res.status(500).json(error.message);
+      next(error);
    }
      
 };
